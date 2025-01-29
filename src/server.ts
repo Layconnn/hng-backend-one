7ba3a30452cc42854c0568a6566ef1c5cc5b8184
Laycon <olamilekanjinadu21@gmail.com>
2025-01-29 20:06:09 +0100

    cors

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from 'cors';
+
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT;
 const currentDateTime = new Date().toISOString();
 
