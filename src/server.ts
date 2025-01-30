bb0f434937b914a472a9ad0b09d089194df7c483
Laycon <olamilekanjinadu21@gmail.com>
2025-01-30 09:58:41 +0100

    dynamic date time

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,18 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
-import cors from 'cors';
-
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT;
-const currentDateTime = new Date().toISOString();
 
+const PORT = process.env.PORT || 3000; // Fallback port if not set
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({
     email: process.env.EMAIL, // Set this in a .env file
-    current_datetime: currentDateTime,
+    current_datetime: new Date().toISOString(), // Dynamic timestamp
     github_url: process.env.GITHUB,
   });
 });
